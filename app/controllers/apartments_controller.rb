ec3e346d6cd6f58d242b4c8c75d8ea10565997dc
Ivan Carrasco <alexcct@hotmail.com>
2021-06-23 15:28:51 -0400

    Rutas anidadas

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,19 +1,24 @@
 class ApartmentsController < ApplicationController
     def index
-        @apartments = Apartment.all    
+        @building = Building.find params[:building_id]
+        @apartments = @building.apartments # Se muestran los apartments asociados a este building
     end
 
-    def new
-        @apartment = Apartment.new
-        @buildings = Building.all    
-    end
+        def new
+            @building = Building.find params[:building_id]
+            @apartment = Apartment.new
+         end
+   
+    
 
     def create
+        @building = Building.find params[:building_id]
         @apartment = Apartment.new(apartment_params) 
         # Strong parameters, previene ataques de mass assignment
+        @apartment.building = @building #Asignamos el building, ya no es necesario pasarlo en el formulario, ahora viene en la url
         respond_to do |format|
             if @apartment.save
-                format.html { redirect_to @apartment, notice: 'Apartment was
+                format.html { redirect_to building_apartment_path(@building, @apartment), notice: 'Apartment was
                     successfully created.' }
                 else
                     format.html { render :new }
@@ -22,12 +27,38 @@ class ApartmentsController < ApplicationController
         end
 
         def show
+            @building = Building.find params[:building_id]
             @apartment = Apartment.find(params[:id])
+        end
+
+        def edit
+            @building = Building.find params[:building_id]
+            @apartment = Apartment.find params[:id]
+        end
+
+        def update
+            @building = Building.find params[:building_id]
+            @apartment = Apartment.find params[:id]
+            respond_to do |format|
+                if @apartment.update(apartment_params.merge(building: @building))# Se añade el building que se obtuvo en la llamada a find_building
+                    format.html { redirect_to building_apartment_path(@building, @apartment), notice: 'Apartment was successfully updated.' }
+                else
+                    format.html { render :edit }
+                end
+            end
+        end
+
+        def destroy
+            @apartment = Apartment.find params[:id]
+            @apartment.destroy
+            respond_to do |format|
+                format.html { redirect_to building_apartments_url, notice: 'Apartment was successfully destroyed.' }
+            end
         end
         
         private
         def apartment_params
-            params.require(:apartment).permit(:number, :building_id)
+            params.require(:apartment).permit(:number)
         end
 
     
